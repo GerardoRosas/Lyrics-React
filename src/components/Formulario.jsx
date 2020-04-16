b8b299a29a7aadae47ef0cba415d2a6e74142fa6
FreezeByte <rosas.ruizlg@gmail.com>
2020-04-15 20:01:43 -0500

    Componentes de formulario y letras

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+const Formulario = ({guardarBusquedaLetra}) => {
+
+    const [ busqueda, guardarBusqueda ] = useState({
+        artista: '',
+        cancion: ''
+    })
+
+    const { artista, cancion } = busqueda; 
+
+    const [ error, guardarError ] = useState(false);
+
+    //Funcion a cada input para leer su contenido
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //Consultar las APIS
+    const buscarInformacion = e => {
+        e.preventDefault();
+        if(artista.trim() === '' || cancion.trim() === ''){
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+        //Pasando al componente principal
+        guardarBusquedaLetra(busqueda);
+    }
+
+
+    return ( 
+        <div className="bg-info">
+            {error ? <p className="alert alert-danger text-center p-2">
+                        Todos los campos son obligatorios</p> : null }
+            <div className="container">
+                <div className="row">
+                    
+                    <form
+                        onSubmit={buscarInformacion}
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-3"
+                    >
+                        <fieldset>
+                           <legend className="text-center">Buscador Letras Canciones
+                           </legend>
+
+                           <div className="row">
+                               <div className="col-md-6">
+                                   <div className="form-group">
+                                       <label>Artista</label>
+                                       <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            placeholder="Nombre del Artista"
+                                            onChange={actualizarState}
+                                       />
+                                   </div>                                   
+                               </div>
+
+                               <div className="col-md-6">
+                                    <div className="form-group">
+                                       <label>Canción</label>
+                                       <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Nombre del Canción"
+                                            onChange={actualizarState}
+                                       />
+                                   </div>
+                               </div>
+                           </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Buscar</button>
+
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default Formulario;
